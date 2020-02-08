5afaaaf39d7e74deb0c6046a944828f54f823311
mbcod3 <58457269+mbcod3@users.noreply.github.com>
2020-02-08 19:59:30 +0500

    Updates

---
+++ precache-manifest.2bf57d4388ecd935c065c9052741259d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f7dc8a9acf7ab0da05a0c60b7caca0",
+    "url": "/rest-countries-api-with-theme-switcher/index.html"
+  },
+  {
+    "revision": "f110d74c15af18f158f0",
+    "url": "/rest-countries-api-with-theme-switcher/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2527780096f1bcbf84bc",
+    "url": "/rest-countries-api-with-theme-switcher/static/js/2.71c92afc.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/rest-countries-api-with-theme-switcher/static/js/2.71c92afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f110d74c15af18f158f0",
+    "url": "/rest-countries-api-with-theme-switcher/static/js/main.0143f93f.chunk.js"
+  },
+  {
+    "revision": "c6398b4924592596ace7",
+    "url": "/rest-countries-api-with-theme-switcher/static/js/runtime-main.0f55a7bb.js"
+  }
+]);
